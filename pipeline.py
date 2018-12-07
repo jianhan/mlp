7b911a806c2eeca237ee8039c946bbe9670e75a9
Jian Han <jian.han@mail.com>
2018-12-07 21:36:23 +1000

    [feat] Added correlation chart

--- pipeline.py
+++ pipeline.py
@@ -11,10 +11,12 @@ from textblob import Word
 from sklearn.feature_extraction.text import TfidfVectorizer
 import seaborn as sn
 
+
 class Pipeline:
 
     def __init__(self):
-        self.df = pd.read_csv('dataset.csv', engine='python', error_bad_lines=False)
+        self.df = pd.read_csv(
+            'dataset.csv', engine='python', error_bad_lines=False)
 
     def run(self):
         self.__wrangling()
@@ -46,7 +48,7 @@ class Pipeline:
         # general summary
         print("General Stats::")
         print(self.df.info())
-        print("Summary Stats::" )
+        print("Summary Stats::")
         print(self.df.describe())
 
         # print sample data
@@ -54,7 +56,8 @@ class Pipeline:
 
     def __wrangling(self):
         # type casting
-        self.df['delivery_date'] = pd.to_datetime(self.df.delivery_date, errors="coerce")
+        self.df['delivery_date'] = pd.to_datetime(
+            self.df.delivery_date, errors="coerce")
 
         # drop null
         self.df.dropna(inplace=True)
@@ -63,55 +66,77 @@ class Pipeline:
         self.df['d_year'] = self.df['delivery_date'].apply(lambda d: d.year)
         self.df['d_month'] = self.df['delivery_date'].apply(lambda d: d.month)
         self.df['d_day'] = self.df['delivery_date'].apply(lambda d: d.day)
-        self.df['d_day_of_week'] = self.df['delivery_date'].apply(lambda d: d.dayofweek)
-        self.df['d_day_name'] = self.df['delivery_date'].apply(lambda d: d.day_name)
-        self.df['d_day_of_year'] = self.df['delivery_date'].apply(lambda d: d.dayofyear)
-        self.df['d_week_of_year'] = self.df['delivery_date'].apply(lambda d: d.weekofyear)
-        self.df['d_quarter'] = self.df['delivery_date'].apply(lambda d: d.quarter)
+        self.df['d_day_of_week'] = self.df['delivery_date'].apply(
+            lambda d: d.dayofweek)
+        self.df['d_day_name'] = self.df['delivery_date'].apply(
+            lambda d: d.day_name)
+        self.df['d_day_of_year'] = self.df['delivery_date'].apply(
+            lambda d: d.dayofyear)
+        self.df['d_week_of_year'] = self.df['delivery_date'].apply(
+            lambda d: d.weekofyear)
+        self.df['d_quarter'] = self.df['delivery_date'].apply(
+            lambda d: d.quarter)
 
         # title feature engineering
-        self.df['title_word_count'] = self.df['title'].apply(lambda x: len(str(x).split(" ")))
+        self.df['title_word_count'] = self.df['title'].apply(
+            lambda x: len(str(x).split(" ")))
         self.df['title_char_count'] = self.df['title'].str.len()
 
     def __visualization(self):
         # overall shop and qty sub plot
-        # fig,ax = plt.subplots() 
-        # sn.pointplot(data=self.df[['qty', 'd_day_of_week', 'shop_id']],x='d_day_of_week', y='qty', hue='shop_id', ax=ax)
+
+        # fig,ax = plt.subplots()
+        # sn.pointplot(data=self.df[['qty', 'd_day_of_week', 'shop_id']],x='d_day_of_week', y='qty', hue='shop_id', ax=ax, estimator=sum)
         # ax.set(title="Shop wise qty distribution of counts")
 
-        fig,ax = plt.subplots() 
-        sn.barplot(data=self.df[['d_month', 'qty']],x='d_month', y='qty', estimator=sum)
-        ax.set(title="Month wise qty distribution of counts")
+        # fig,ax = plt.subplots()
+        # sn.barplot(data=self.df[['d_month', 'qty']],x='d_month', y='qty', estimator=sum)
+        # ax.set(title="Month wise qty distribution of counts")
+
+        corrMatt = self.df[["d_month", "d_day", "d_day_of_week",
+                            "price", "d_week_of_year", "shop_id", "delivery_date","qty"]].corr()
+        mask = np.array(corrMatt)
+        mask[np.tril_indices_from(mask)] = False
+        sn.heatmap(corrMatt, mask=mask, vmax=.8, square=True, annot=True)
+
         plt.show()
 
     def __feature_engineering(self):
         # one hot encoding
-        self.df = pd.get_dummies(self.df, prefix=['sku'], columns=['sku']) 
-        self.df = pd.get_dummies(self.df, prefix=['delivery_zone'], columns=['delivery_zone']) 
-        self.df = pd.get_dummies(self.df, prefix=['shop_id'], columns=['shop_id']) 
+        self.df = pd.get_dummies(self.df, prefix=['sku'], columns=['sku'])
+        self.df = pd.get_dummies(
+            self.df, prefix=['delivery_zone'], columns=['delivery_zone'])
+        self.df = pd.get_dummies(
+            self.df, prefix=['shop_id'], columns=['shop_id'])
 
         # title remove special characters
-        self.df['title'] = self.df['title'].apply(lambda x: re.sub(r'\W+', '', x))
+        self.df['title'] = self.df['title'].apply(
+            lambda x: re.sub(r'\W+', '', x))
 
         # title lowercase
-        self.df['title'] = self.df['title'].apply(lambda x: " ".join(x.lower() for x in x.split()))
+        self.df['title'] = self.df['title'].apply(
+            lambda x: " ".join(x.lower() for x in x.split()))
 
         # title unescape html
         self.df['title'] = self.df['title'].apply(lambda x: html.unescape(x))
 
         # title removing punctuation
-        self.df['title'] = self.df['title'].str.replace('[^\w\s]','')
+        self.df['title'] = self.df['title'].str.replace('[^\w\s]', '')
 
         # title remove stop words
         stop = stopwords.words('english')
-        self.df['title'] = self.df['title'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+        self.df['title'] = self.df['title'].apply(
+            lambda x: " ".join(x for x in x.split() if x not in stop))
 
-        # title lemmatization 
-        self.df['title'] = self.df['title'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+        # title lemmatization
+        self.df['title'] = self.df['title'].apply(
+            lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
 
         # title vectorization
-        tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
-        self.df['title_vect'] = list(tfidf.fit_transform(self.df.title).toarray())
+        tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2',
+                                encoding='latin-1', ngram_range=(1, 2), stop_words='english')
+        self.df['title_vect'] = list(
+            tfidf.fit_transform(self.df.title).toarray())
 
     def __feature_scaling(self):
         pass
